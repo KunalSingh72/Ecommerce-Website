beebc6f180a658189b143c586fb5890d9b5be897
KunalSingh72 <kunalsiinghrathore@gmail.com>
2026-04-25 15:53:28 +0530

    Add server proxy config for /api and /images

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -14,13 +14,13 @@ export function HomePage() {
   });
   */
   useEffect(() => {
-    axios.get("http://localhost:3000/api/products").then((response) => {
+    axios.get("/api/products").then((response) => {
       setProducts(response.data);
     });
   }, []);
 
   useEffect(() => {
-    axios.get("http://localhost:3000/api/cart-items").then((response) => {
+    axios.get("/api/cart-items").then((response) => {
       setCart(response.data);
     });
   }, []);
