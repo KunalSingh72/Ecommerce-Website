95a20dc94ff988da941e1bad40deec44fdb6e6e5
KunalSingh72 <kunalsiinghrathore@gmail.com>
2026-04-27 14:31:31 +0530

    Use beforeEach to remove duplication

--- src/pages/home/Product.test.jsx
+++ src/pages/home/Product.test.jsx
@@ -1,23 +1,31 @@
-import { it, expect, describe, vi } from "vitest";
+import { it, expect, describe, vi, beforeEach } from "vitest";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import axios from "axios";
 import { Product } from "./Product";
 
 vi.mock("axios");
-const product = {
-  id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-  image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-  name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-  rating: {
-    stars: 4.5,
-    count: 87,
-  },
-  priceCents: 1090,
-  keywords: ["socks", "sports", "apparel"],
-};
-const loadCart = vi.fn();
+
 describe("Product component", () => {
+
+  let product;
+  let loadCart = vi.fn();
+  beforeEach(() => {
+    product = {
+      id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+      image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+      name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+      rating: {
+        stars: 4.5,
+        count: 87,
+      },
+      priceCents: 1090,
+      keywords: ["socks", "sports", "apparel"],
+    };
+
+    loadCart = vi.fn();
+  })
+
   it("displays the product details correctly", () => {
     render(<Product product={product} loadCart={loadCart} />);
 
