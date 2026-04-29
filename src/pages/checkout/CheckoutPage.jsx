503d4aba064b1abb8702a700a2431965edb7144e
KunalSingh72 <kunalsiinghrathore@gmail.com>
2026-04-29 14:33:25 +0530

    fixed OrderSummary naming

--- src/pages/checkout/CheckoutPage.jsx
+++ src/pages/checkout/CheckoutPage.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
 import { CheckoutHeader } from "./CheckoutHeader";
-import { OrderSummary } from "./orderSummary";
+import { OrderSummary } from "./OrderSummary";
 import { PaymentSummary } from "./PaymentSummary";
 import "./checkout-header.css";
 import "./CheckoutPage.css";
